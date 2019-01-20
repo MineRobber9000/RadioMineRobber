4f21fbc59aec02fbecd7cb101fdc66d1124ff5e7
khuxkm fbexl <khuxkm@ttm.sh>
2019-01-19 23:01:24 -0500

    Move to using MP3 as opposed to Ogg

--- broadcaster.py
+++ broadcaster.py
@@ -10,9 +10,9 @@ if sys.argv[1] not in config.sections():
 	sys.exit(127)
 station=config[sys.argv[1]]
 
-files = ["bumps/bump-00000.ogg","bumps/bump-00002.ogg","music/panicatthedisco-highhopes.ogg","bumps/bump-00001.ogg"]
+files = ["bumps/bump-00000.mp3","bumps/bump-00002.mp3","music/panicatthedisco-highhopes.mp3","bumps/bump-00001.mp3"]
 
 with open("tracks.pls","w") as f:
 	f.write("\n".join(files))
 
-subprocess.run(["/usr/bin/liquidsoap",'output.icecast(%vorbis,host="{station[host]}",port={station[port]},mount="{station[mount]}",user="{station[user]}",password="{station[password]}",mksafe(playlist.once("tracks.pls")),on_stop=shutdown)'.format(station=station)])
+subprocess.run(["/usr/bin/liquidsoap",'output.icecast(%mp3,host="{station[host]}",port={station[port]},mount="{station[mount]}",user="{station[user]}",password="{station[password]}",mksafe(playlist.once("tracks.pls")),on_stop=shutdown)'.format(station=station)])
