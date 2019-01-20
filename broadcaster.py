7acd3964872b41c082f33857c3ec0fc162f08921
khuxkm fbexl <khuxkm@ttm.sh>
2019-01-19 22:08:02 -0500

    Add High Hopes by Panic at the Disco
    Also replace old, broken script with liquidsoap wrapper

--- broadcaster.py
+++ broadcaster.py
@@ -1,31 +1,18 @@
-import shouty, time, sys
-from tinytag import TinyTag
+import subprocess,sys
 from configparser import ConfigParser
-
 config = ConfigParser()
 config.read("config.ini")
 if len(sys.argv)!=2:
 	print("Usage: python3 broadcaster.py <station>")
 	sys.exit(1)
 if sys.argv[1] not in config.sections():
-	print("Error: Invalid station {!r}! (Did you forget to configure it?)")
+	print("Error: Invalid section {!r}! (Did you forget to add a configuration section for it?)")
 	sys.exit(127)
-config = config[sys.argv[1]]
-params = {}
-for k in config.keys():
-	params[k]=config[k]
-	if k=="port":
-		params[k]=int(params[k])
-	elif k=="format":
-		params[k]=getattr(shouty.Format,params[k].upper())
+station=config[sys.argv[1]]
 
-with open("playlist.txt") as f:
-	files = [l.strip() for l in f if l.strip()]
-metadata = [TinyTag.get(f) for f in files]
+files = ["bumps/bump-00000.ogg","bumps/bump-00002.ogg","music/panicatthedisco-highhopes.ogg","bumps/bump-00001.ogg"]
 
-with shouty.connect(**params) as c:
-	for i in range(len(files)):
-		c.send_file(files[i])
-		c.sync()
-		print("Sent {}".format(files[i]))
-		time.sleep(metadata[i].duration)
+with open("tracks.pls","w") as f:
+	f.write("\n".join(files))
+
+subprocess.run(["/usr/bin/liquidsoap",'output.icecast(%vorbis,host="{station[host]}",port={station[port]},mount="{station[mount]}",user="{station[user]}",password="{station[password]}",mksafe(playlist.once("tracks.pls")),on_stop=shutdown)'.format(station=station)])
